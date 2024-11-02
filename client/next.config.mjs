7cbc4306d6da4fb27cb2e463955991176ff417dc
tonybuii2003 <longphibui1993@gmail.com>
2024-11-02 16:46:58 -0400

    update path

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "/client",
-    output: "export",  // <=== enables static exports
-    reactStrictMode: true,
 };
 
 export default nextConfig;
